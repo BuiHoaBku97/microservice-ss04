d2c194b2b0c9726b14b36d967d992d4407042d77
BuiHoaBku97 <vanhoabku97@gmail.com>
2026-08-27 22:05:20 +0700

    - api-gateway filter

---
+++ api-gateway/src/main/java/startup/vn/apigateway/Logs/LoggingFilter.java
@@ -0,0 +1,23 @@
+package startup.vn.apigateway.Logs;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().value();
+        System.out.println("Incoming request to: " + path);
+        return chain.filter(exchange);
+    }
+}
